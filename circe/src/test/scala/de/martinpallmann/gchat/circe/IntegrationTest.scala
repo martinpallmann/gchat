0f6cc7c6b89aed5771aff13e64a6e15ee8b7989c
Martin Pallmann <sayhello@martinpallmann.de>
2020-05-02 19:21:37 +0200

    fix not compiling test

--- circe/src/test/scala/de/martinpallmann/gchat/circe/IntegrationTest.scala
+++ circe/src/test/scala/de/martinpallmann/gchat/circe/IntegrationTest.scala
@@ -1,15 +1,15 @@
 package de.martinpallmann.gchat.circe
 
-import de.martinpallmann.gchat.Event
+import de.martinpallmann.gchat.BotRequest
 import de.martinpallmann.gchat.tck.Tck
 import io.circe.Json
 import io.circe.parser._
 
 import scala.util.Try
 
-class IntegrationTest extends Tck[Json] with EventDecoder {
+class IntegrationTest extends Tck[Json] with BotRequestDecoder {
 
   def parseJson: String => Try[Json] = parse(_).toTry
 
-  def decode: Json => Try[Event] = _.as[Event](decodeEvent).toTry
+  def decode: Json => Try[BotRequest] = _.as[BotRequest](decodeBotRequest).toTry
 }
