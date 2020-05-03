e712c91eb813d06e5b97e8e0c382b49c2db5c1a9
Martin Pallmann <sayhello@martinpallmann.de>
2020-05-03 14:45:51 +0200

    make it a bit safer

--- project/MavenCentral.scala
+++ project/MavenCentral.scala
@@ -1,24 +1,34 @@
 import java.net.URL
 import java.io.{BufferedReader, InputStreamReader}
+
 import io.circe.parser._
 
-object MavenCentral {
-  def version: String = {
-    val url = new URL(
-      "https://search.maven.org/solrsearch/select?q=de.martinpallmann.gchat&rows=1"
-    )
-    val in = new BufferedReader(new InputStreamReader(url.openStream))
+import scala.util.Try
 
-    @scala.annotation.tailrec
-    def read(acc: String = ""): String =
-      in.readLine match {
-        case null => acc
-        case s    => read(s"$acc\n$s")
-      }
-    val result = read()
-    in.close()
-    extractVersion(result).getOrElse("x.x.x")
-  }
+object MavenCentral {
+  def version: String =
+    (for {
+      s <- readFromMaven
+      res <- extractVersion(s)
+    } yield res).getOrElse("x.x.x")
+
+  private def readFromMaven: Option[String] =
+    Try {
+      val url = new URL(
+        "https://search.maven.org/solrsearch/select?q=de.martinpallmann.gchat&rows=1"
+      )
+      val in = new BufferedReader(new InputStreamReader(url.openStream))
+
+      @scala.annotation.tailrec
+      def read(acc: String = ""): String =
+        in.readLine match {
+          case null => acc
+          case s    => read(s"$acc\n$s")
+        }
+      val result = read()
+      in.close()
+      result
+    }.toOption
 
   private def extractVersion(s: String): Option[String] =
     for {
